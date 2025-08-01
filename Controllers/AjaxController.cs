b04183b1e568a189a296d3b718e57f745b7535d5
Edita Latifi <108685476+EditaLatifi@users.noreply.github.com>
2025-08-01 21:06:07 +0200

    Add files via upload

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Finlab.Controllers
+{
+	public class AjaxController : Controller
+	{
+		public IActionResult Activity()
+		{
+			return View();
+		}
+		public IActionResult EventAgenda()
+		{
+			return View();
+		}
+		public IActionResult FeaturedCompanies()
+		{
+			return View();
+		}
+		public IActionResult Message()
+		{
+			return View();
+		}
+		public IActionResult RecentActivity()
+		{
+			return View();
+		}
+	}
+}
---
+++ Controllers/FinlabController.cs
@@ -0,0 +1,365 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Finlab.Controllers
+{
+	public class FinlabController : Controller
+	{
+		public IActionResult Activity()
+		{
+			return View();
+		}
+		public IActionResult AddBlog()
+		{
+			return View();
+		}
+		public IActionResult AddEmail()
+		{
+			return View();
+		}
+		public IActionResult AppCalender()
+		{
+			return View();
+		}
+		public IActionResult AppProfile()
+		{
+			return View();
+		}
+		public IActionResult Banking()
+		{
+			return View();
+		}
+		public IActionResult Blog()
+		{
+			return View();
+		}
+		public IActionResult BlogCategory()
+		{
+			return View();
+		}
+		public IActionResult Celandar()
+		{
+			return View();
+		}
+		public IActionResult ChartChartist()
+		{
+			return View();
+		}
+		public IActionResult ChartChartjs()
+		{
+			return View();
+		}
+		public IActionResult ChartFlot()
+		{
+			return View();
+		}
+		public IActionResult ChartMorris()
+		{
+			return View();
+		}
+		public IActionResult ChartPeity()
+		{
+			return View();
+		}
+		public IActionResult ChartSparkline()
+		{
+			return View();
+		}
+		public IActionResult Chat()
+		{
+			return View();
+		}
+		public IActionResult CoinDetails()
+		{
+			return View();
+		}
+		public IActionResult Contact()
+		{
+			return View();
+		}
+		public IActionResult Content()
+		{
+			return View();
+		}
+		public IActionResult ContentAdd()
+		{
+			return View();
+		}
+		public IActionResult CourseDetails()
+		{
+			return View();
+		}
+		public IActionResult CourseListing()
+		{
+			return View();
+		}
+		public IActionResult Crypto()
+		{
+			return View();
+		}
+		public IActionResult EcomCheckout()
+		{
+			return View();
+		}
+		public IActionResult EcomCustomers()
+		{
+			return View();
+		}
+		public IActionResult EcomInvoice()
+		{
+			return View();
+		}
+		public IActionResult EcomProductDetail()
+		{
+			return View();
+		}
+		public IActionResult EcomProductGrid()
+		{
+			return View();
+		}
+		public IActionResult EcomProductList()
+		{
+			return View();
+		}
+		public IActionResult EcomProductOrder()
+		{
+			return View();
+		}
+		public IActionResult EditProfile()
+		{
+			return View();
+		}
+		public IActionResult EmailCompose()
+		{
+			return View();
+		}
+		public IActionResult EmailInbox()
+		{
+			return View();
+		}
+		public IActionResult EmailRead()
+		{
+			return View();
+		}
+		public IActionResult EmailTemplate()
+		{
+			return View();
+		}
+		public IActionResult EmptyPage()
+		{
+			return View();
+		}
+		public IActionResult FileManager()
+		{
+			return View();
+		}
+		public IActionResult FormCkeditor()
+		{
+			return View();
+		}
+		public IActionResult FormElement()
+		{
+			return View();
+		}
+		public IActionResult FormPickers()
+		{
+			return View();
+		}
+		public IActionResult FormValidation()
+		{
+			return View();
+		}
+		public IActionResult FormWizard()
+		{
+			return View();
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Index2()
+		{
+			return View();
+		}
+		public IActionResult Invoice()
+		{
+			return View();
+		}
+		public IActionResult Kanban()
+		{
+			return View();
+		}
+		public IActionResult MapJqvmap()
+		{
+			return View();
+		}
+		public IActionResult Market()
+		{
+			return View();
+		}
+		public IActionResult Menu()
+		{
+			return View();
+		}
+		public IActionResult PageError400()
+		{
+			return View();
+		}
+		public IActionResult PageError403()
+		{
+			return View();
+		}
+		public IActionResult PageError404()
+		{
+			return View();
+		}
+		public IActionResult PageError500()
+		{
+			return View();
+		}
+		public IActionResult PageError503()
+		{
+			return View();
+		}
+		public IActionResult PageForgotPassword()
+		{
+			return View();
+		}
+		public IActionResult PageLockScreen()
+		{
+			return View();
+		}
+		public IActionResult PageLogin()
+		{
+			return View();
+		}
+		public IActionResult PageRegister()
+		{
+			return View();
+		}
+		public IActionResult PostDetails()
+		{
+			return View();
+		}
+		public IActionResult TableBootstrapBasic()
+		{
+			return View();
+		}
+		public IActionResult TableDatatableBasic()
+		{
+			return View();
+		}
+		public IActionResult Ticketing()
+		{
+			return View();
+		}
+		public IActionResult ToDoList()
+		{
+			return View();
+		}
+		public IActionResult UcLightgallery()
+		{
+			return View();
+		}
+		public IActionResult UcNestable()
+		{
+			return View();
+		}
+		public IActionResult UcNOUISlider()
+		{
+			return View();
+		}
+		public IActionResult UcSelect2()
+		{
+			return View();
+		}
+		public IActionResult UcSweetalert()
+		{
+			return View();
+		}
+		public IActionResult UcToastr()
+		{
+			return View();
+		}
+		public IActionResult UIAccordion()
+		{
+			return View();
+		}
+		public IActionResult UIAlert()
+		{
+			return View();
+		}
+		public IActionResult UIBadge()
+		{
+			return View();
+		}
+		public IActionResult UIButton()
+		{
+			return View();
+		}
+		public IActionResult UIButtonGroup()
+		{
+			return View();
+		}
+		public IActionResult UICard()
+		{
+			return View();
+		}
+		public IActionResult UICarousel()
+		{
+			return View();
+		}
+		public IActionResult UIDropdown()
+		{
+			return View();
+		}
+		public IActionResult UIGrid()
+		{
+			return View();
+		}
+		public IActionResult UIListGroup()
+		{
+			return View();
+		}
+		public IActionResult UIModal()
+		{
+			return View();
+		}
+		public IActionResult UIPagination()
+		{
+			return View();
+		}
+		public IActionResult UIPopover()
+		{
+			return View();
+		}
+		public IActionResult UIProgressbar()
+		{
+			return View();
+		}
+		public IActionResult UITab()
+		{
+			return View();
+		}
+		public IActionResult UITypography()
+		{
+			return View();
+		}
+		public IActionResult Users()
+		{
+			return View();
+		}
+		public IActionResult WidgetCard()
+		{
+			return View();
+		}
+		public IActionResult WidgetChart()
+		{
+			return View();
+		}
+		public IActionResult WidgetList()
+		{
+			return View();
+		}
+	}
+}
